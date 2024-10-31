f539ba06f4cb275b6ef0eff239db369485379305
Ashok Tamang <ashoktamang1108@gmail.com>
2024-10-30 20:59:30 -0400

    feat: ipaddress, lanuage, software api request

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//Request header parser microservice 
+app.get('/api/whoami', function (req, res){
+  console.log(req.headers['x-forwarded-for']);
+  res.json({ipaddress: `${req.ip}`, lanuage: `${req.headers['accept-language']}`, software: `${req.headers['user-agent']}`})
+} );
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
