f2715d0d787dfe1c466f33a870dbe39052c979af
Ashok Tamang <ashoktamang1108@gmail.com>
2024-10-30 21:03:22 -0400

    fix: lanuage key

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/api/hello', function (req, res) {
 //Request header parser microservice 
 app.get('/api/whoami', function (req, res){
   console.log(req.headers['x-forwarded-for']);
-  res.json({ipaddress: `${req.ip}`, lanuage: `${req.headers['accept-language']}`, software: `${req.headers['user-agent']}`})
+  res.json({ipaddress: `${req.ip}`, language: `${req.headers['accept-language']}`, software: `${req.headers['user-agent']}`})
 } );
 
 // listen for requests :)
